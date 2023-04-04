e05eb0d12afb60d73011eec8b8d8dd6a16390d6a
lofilauren <89169125+lofilauren@users.noreply.github.com>
2023-04-04 13:24:46 -0700

    Moved Date and Description under Header
    Moved date and description under the header. adjusted CSS for humidity and wind speed. Fixed temperature conversion link.

--- src/script.js
+++ src/script.js
@@ -77,11 +77,11 @@ function displayFahrenheitTemperature(event) {
 }
 
 function displaycelsiusTemperature(event) {
-  event.preventDefault;
+  event.preventDefault();
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remvove("active");
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = celsiusTemperature;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let celsiusTemperature = null;
@@ -91,3 +91,6 @@ searchBar.addEventListener("submit", search);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
