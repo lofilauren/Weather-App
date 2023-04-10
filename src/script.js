73741bfed1d1da055159e4295278ed5909ab4530
lofilauren <89169125+lofilauren@users.noreply.github.com>
2023-04-09 20:24:15 -0700

    Temperature Conversion Troubleshooting

--- src/script.js
+++ src/script.js
@@ -113,12 +113,10 @@ function search(event) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  console.log("Fahrenheit temperature:", fahrenheitTemperature);
 }
 
 function displaycelsiusTemperature(event) {
